4d2c8bfc745e32d7c3181493a53b8117da0f61d5
bomblozaurus <49494671+bomblozaurus@users.noreply.github.com>
2019-12-26 01:21:35 +0100

    Changed events repo to JpaRepository to be able to fetch data in pages currently disabled EventsController (removed /events path)

--- mobile-api/src/main/java/com/teamE/events/controllers/EventsController.java
+++ mobile-api/src/main/java/com/teamE/events/controllers/EventsController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/events")
+//@RequestMapping("/events")
 public class EventsController {
 
     private EventManager eventManager;
