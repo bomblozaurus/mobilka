5d1a878d5b92f81afbb4305eb5dc053b9f5c60b0
AdamGniadzik <56366799+AdamGniadzik@users.noreply.github.com>
2020-01-15 19:31:42 +0100

    Poprawki

--- mobile-api/src/main/java/com/teamE/users/UserToUserTokenInformationTransformer.java
+++ mobile-api/src/main/java/com/teamE/users/UserToUserTokenInformationTransformer.java
@@ -1,21 +1,17 @@
 package com.teamE.users;
 
 import com.teamE.common.Transformer;
-import org.apache.tomcat.util.buf.StringUtils;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class UserToUserTokenInformationTransformer implements Transformer<User, UserTokenInformation> {
     @Override
     public UserTokenInformation transform(User user) {
-        List<String> roles = user.getUserRoles().stream().map(Object::toString).collect(Collectors.toList());
+        UserRole role = user.getUserRole();
         return UserTokenInformation.builder()
                 .email(user.getEmail())
                 .name(user.getFirstName() + " " + user.getLastName())
-                .role(StringUtils.join(roles, ','))
+                .role(role.getType().name())
                 .build();
     }
 }
