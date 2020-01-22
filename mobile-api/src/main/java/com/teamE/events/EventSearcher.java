afa9456b3dbba621ef3dadc4cf225ed75b01c0f9
rjehf <45949354+rjehf@users.noreply.github.com>
2020-01-22 19:03:29 +0100

    Start rebuild searcher

--- mobile-api/src/main/java/com/teamE/events/EventSearcher.java
+++ mobile-api/src/main/java/com/teamE/events/EventSearcher.java
@@ -6,6 +6,7 @@ import com.teamE.users.StudentHouse;
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.FullTextQuery;
 import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.BooleanJunction;
 import org.hibernate.search.query.dsl.MustJunction;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Page;
@@ -19,6 +20,7 @@ import javax.persistence.PersistenceContext;
 
 import org.apache.lucene.search.Query;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -28,28 +30,36 @@ public class EventSearcher {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public Page<Event> searchEvent(Scope scope, StudentHouse studentHouse, String text, Pageable page) {
+    public Page<Event> searchEvent(Scope scope, StudentHouse studentHouse, String query, Pageable page) {
 
         Query keywordQuery;
         QueryBuilder queryBuilder = getQueryBuilder();
+
+        BooleanJunction<BooleanJunction> booleanJunctionBooleanJunction = queryBuilder
+                .bool();
+
+        List<Query> queries = new ArrayList<>();
+
+        String text = query.trim();
+        if (text.length() > 0) {
+            queries.add(checkQuery(text));
+        }
+
+
         MustJunction mustJunction = queryBuilder
-                .bool().must(queryBuilder
-                        .bool().should(queryBuilder.phrase()
-                                .withSlop(1).onField("name")
-                                .sentence(text).createQuery())
-                        .should(queryBuilder.phrase()
-                                .withSlop(1).onField("street")
-                                .sentence(text).createQuery())
-                        .should(queryBuilder.phrase()
-                                .withSlop(1).onField("description")
-                                .sentence(text).createQuery())
-                        .should(queryBuilder.phrase()
-                                .withSlop(1).onField("city")
-                                .sentence(text).createQuery()).createQuery())
+                .bool()
+                .must(checkQuery(query))
                 .must(queryBuilder.keyword()
                         .onField("scope")
                         .matching(scope)
                         .createQuery());
+
+        if (scope != Scope.DORMITORY) {
+            keywordQuery = mustJunction.must(queryBuilder.keyword()
+                    .onField("scope")
+                    .matching(scope)
+                    .createQuery()).createQuery();
+        }
 
         if (studentHouse != null) {
             keywordQuery = mustJunction.must(queryBuilder.keyword()
@@ -65,6 +75,62 @@ public class EventSearcher {
         long total = getJpaQuery(keywordQuery, page).getResultSize();
         return new PageImpl<>(events, page, total);
     }
+
+
+    Query checkQuery(String query) {
+        QueryBuilder queryBuilder = getQueryBuilder();
+        return queryBuilder
+                .bool()
+                .must(queryBuilder
+                        .bool().should(queryBuilder.phrase()
+                                .withSlop(1).onField("name")
+                                .sentence(query).createQuery())
+                        .should(queryBuilder.phrase()
+                                .withSlop(1).onField("street")
+                                .sentence(query).createQuery())
+                        .should(queryBuilder.phrase()
+                                .withSlop(1).onField("description")
+                                .sentence(query).createQuery())
+                        .should(queryBuilder.phrase()
+                                .withSlop(1).onField("city")
+                                .sentence(query).createQuery()).createQuery()).createQuery();
+    }
+
+    Query checkScope(Scope scope) {
+        QueryBuilder queryBuilder = getQueryBuilder();
+
+        if (scope == Scope.OTHER) {
+            return queryBuilder
+                    .bool().must(queryBuilder.keyword()
+                            .onField("scope")
+                            .matching(Scope.OTHER)
+                            .createQuery()).createQuery();
+        }
+        if (scope == Scope.STUDENT) {
+            return queryBuilder
+                    .bool().must(queryBuilder.bool()
+                            .should(queryBuilder.keyword()
+                                    .onField("scope")
+                                    .matching(Scope.OTHER).createQuery())
+                            .should(queryBuilder.keyword()
+                                    .onField("scope")
+                                    .matching(Scope.STUDENT).createQuery())
+                            .createQuery()).createQuery();
+        }
+        return queryBuilder
+                .bool().must(queryBuilder.bool()
+                        .should(queryBuilder.keyword()
+                                .onField("scope")
+                                .matching(Scope.OTHER).createQuery())
+                        .should(queryBuilder.keyword()
+                                .onField("scope")
+                                .matching(Scope.STUDENT).createQuery())
+                        .should(queryBuilder.keyword()
+                                .onField("scope")
+                                .matching(Scope.DORMITORY).createQuery())
+                        .createQuery()).createQuery();
+    }
+
 
     private FullTextQuery getJpaQuery(org.apache.lucene.search.Query luceneQuery, Pageable page) {
 
